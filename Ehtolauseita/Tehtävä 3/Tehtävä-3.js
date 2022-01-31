444255fa5bfd2439373aedcb2f12b33ea24b14ad
jasminjunnonen <90385536+jasminjunnonen@users.noreply.github.com>
2022-01-31 20:12:20 +0200

    Pariton vai parillinen numero

---
+++ Ehtolauseita/Tehtävä 3/Tehtävä-3.js
@@ -0,0 +1,10 @@
+function sort(){
+  var num1 = parseInt(document.getElementById('luku').value);
+
+  if (num1 % 2){
+    alert(num1 + " on pariton numero");
+  }
+  else{
+    alert(num1 + " on parillinen numero");
+  }
+}
