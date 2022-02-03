5d28b799de114776f60af3379982fc8e4dee3bf6
jasminjunnonen <90385536+jasminjunnonen@users.noreply.github.com>
2022-02-03 19:57:10 +0200

    Karkausvuosi

--- Ehtolauseita/Tehtävä 8/Tehtävä-8.js
+++ Ehtolauseita/Tehtävä 8/Tehtävä-8.js
@@ -1,5 +1,5 @@
 function sort(){
-  var num1 = (document.getElementById('numero').value);
+  var num1 = document.getElementById('numero').value;
 
   if ((0 == num1 % 4) && (0 != num1 % 100) || (0 == num1 % 400)){  //&&(and) ||(or)
     document.getElementById('tulokset').innerHTML = ('Vuosi on karkausvuosi');
