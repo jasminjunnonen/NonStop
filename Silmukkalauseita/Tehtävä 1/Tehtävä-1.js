f5270c404c85484d9aebb440ca5bf5d6a4f87b74
jasminjunnonen <90385536+jasminjunnonen@users.noreply.github.com>
2022-02-04 11:03:12 +0200

    Parilliset luvut

--- Silmukkalauseita/Tehtävä 1/Tehtävä-1.js
+++ Silmukkalauseita/Tehtävä 1/Tehtävä-1.js
@@ -7,7 +7,7 @@ function sort(){
   }
   document.getElementById('tulokset').textContent = numerot;
 }
-//textContent
+
  // (var i = 2;) sets a variable before the loop starts. [starts with 2 not 0]
  // (i <= num1;) defines the condition for the loop to run. [ variable will only print out if given number(num1) is bigger than 2 or equal to 2]
  // (i= i+2) increases a value each time the code block in the loop has been executed. [ always increase the printed out value by 2, starting at 2]
