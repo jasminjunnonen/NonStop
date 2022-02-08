6d6242e44f665fec0a4dcd8699ac393694465ed4
jasminjunnonen <90385536+jasminjunnonen@users.noreply.github.com>
2022-02-08 09:37:24 +0200

    HipHeijaa

--- Silmukkalauseita/Tehtävä 5/Tehtävä-5.js
+++ Silmukkalauseita/Tehtävä 5/Tehtävä-5.js
@@ -2,17 +2,17 @@ function sort(){
   var vastaus = "";
 
   for(var i = 1; i <=100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-      vastaus += "HipHeijaa ";
+    if(i % 3 == 0 && i % 5 == 0){   //jos luku on jaollinen 3 ja 5 niin...
+      vastaus += "HipHeijaa ";      //Tulostaa luvun ja 'HipHeijaa' sen viereen
     }
-    else if(i % 3 == 0){
-      vastaus += "Hip ";
+    else if(i % 3 == 0){    //jos luku on jaollinen 3 niin...
+      vastaus += "Hip ";    //Tulostaa luvun ja 'Hip' sen viereen
     }
-    else if(i % 5 == 0){
-      vastaus += "Heijaa ";
+    else if(i % 5 == 0){      //jos luku on jaollinen 5 niin...
+      vastaus += "Heijaa ";   // Tulostaa luvun ja 'Heijaa' sen viereen
     }
     else {
-      vastaus += i + " ";
+      vastaus += i + " ";   //Jos luku ei ole jaollinen 3 tai 5 niin tulostaa vain luvun.
     }
     document.getElementById('tulokset').textContent = vastaus;
   }
